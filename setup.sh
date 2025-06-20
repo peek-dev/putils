fa417befc02853eb71564f492114a319795e96f0
Paul Karhnak <pdevkarhnak@gmail.com>
2025-06-20 11:09:54 -0400

    Debugged setup script install invocations; added note about running with superuser privileges.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
+# NOTE: if installing to /opt/, this script MUST be run with superuser
+# privileges.
+
 UTILS_ROOT="/opt/putils/"
 BIN_PATH="$UTILS_ROOT/bin"
 
 mkdir -p $UTILS_ROOT
-mkdir $BIN_PATH
+mkdir -p $BIN_PATH
 
-install diceware/diceware $BIN_PATH
-install -d diceware/ "$UTILS_ROOT/diceware"
+install -v diceware/diceware $BIN_PATH
+install -v -d diceware/ "$UTILS_ROOT/diceware"
+install -v diceware/* "$UTILS_ROOT/diceware/"
