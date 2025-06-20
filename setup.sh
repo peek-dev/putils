b38a54c9bd3e776d4cc1ecdc86ac9973dd35ee6a
Paul Karhnak <pdevkarhnak@gmail.com>
2025-06-20 11:03:14 -0400

    Added setup/installation script to repo root.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/bash
+
 UTILS_ROOT="/opt/putils/"
 BIN_PATH="$UTILS_ROOT/bin"
 
 mkdir -p $UTILS_ROOT
 mkdir $BIN_PATH
 
-install -t diceware/diceware.py $BIN_PATH
+install diceware/diceware $BIN_PATH
 install -d diceware/ "$UTILS_ROOT/diceware"
